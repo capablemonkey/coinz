b0b5be7a46b5c263be16173dc58981de467dca08
Gordon Zheng <gordon@greenhouse.io>
2016-09-06 22:32:01 -0400

    Define colors for circles

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 'use strict';
 
+const COLORS = {
+  RED: 'red',
+  BLUE: 'blue',
+  GREEN: 'green',
+  YELLOW: 'yellow'
+}
+
 // create a wrapper around native canvas element (with id="c")
 var canvas = new fabric.Canvas('canvas');
 
 class Coin {
-  constructor(row, column) {
-    this.canvasObject = null;
+  constructor(color, row, column) {
+    this.color = color;
     this.row = row;
     this.column = column;
+    this.canvasObject = null;
   }
 
   draw() {
     this.canvasObject = new fabric.Circle({
       left: this.row * 50,
       top: this.column * 50,
-      fill: 'red',
+      fill: this.color,
       radius: 20
     });
 
@@ -22,9 +30,9 @@ class Coin {
   }
 }
 
-var a = new Coin(1, 1);
-var b = new Coin(1, 2);
-var c = new Coin(1, 3);
-a.draw();
-b.draw();
-c.draw();
+var a = new Coin(COLORS.RED, 1, 1);
+var b = new Coin(COLORS.BLUE, 1, 2);
+var c = new Coin(COLORS.GREEN, 1, 3);
+
+var circles = [a, b, c]
+circles.forEach((c) => c.draw())
